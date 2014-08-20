7ca06c4ffe16fa325c1ef60d16473de320f9d8af
Shaun Donachy <donachys@gmail.com>
2014-08-20 08:51:48 +0000

    fix some errors

--- automatonpi/testapp.py
+++ automatonpi/testapp.py
@@ -16,10 +16,10 @@ class TestApp:
 	pygame.joystick.init()
 	joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
 	print joysticks
-	joystick = pygame.joystick.Joystick(joysticks[0])
-    joystick.init()
-    pygame.event.set_allowed(None)
-    pygame.event.set_allowed([pygame.QUIT, pygame.JOYAXISMOTION, pygame.JOYBALLMOTION, pygame.JOYBUTTONDOWN, pygame.JOYBUTTONUP, pygame.JOYHATMOTION) # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
+	joystick = pygame.joystick.Joystick(0)
+    	joystick.init()
+    	pygame.event.set_allowed(None)
+    	pygame.event.set_allowed([pygame.QUIT, pygame.JOYAXISMOTION, pygame.JOYBALLMOTION, pygame.JOYBUTTONDOWN, pygame.JOYBUTTONUP, pygame.JOYHATMOTION]) # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
 	#DISPLAYSURF = pygame.display.set_mode((WINWIDTH, WINHEIGHT))
 	#pygame.display.set_caption('Automaton-Pi')
 	#BASICFONT = pygame.font.Font('freesansbold.ttf', 32)
@@ -35,19 +35,18 @@ class TestApp:
 				axes = joystick.get_numaxes()
 				print "axes {}".format(axes)
 				for i in range(axes):
-            		axis = joystick.get_axis(i)
-            		print "Axis: {} value: {}".format(i, axis)
-            	buttons = joystick.get_numbuttons()
-            	print "buttons {}".format(buttons)
-            	for i in range(buttons):
-            		button = joystick.get_button(i)
-            		print "Button: {} value: {}".format(i, button)
-            	hats = joystick.get_numhats()
-            	print "hats {}".format(hats)
-            	for i in range(hats):
-            		hat = joystick.get_hat(i)
-            		print "Hat: {} value: {}",format(i, hat)
-            	print 
+            				axis = joystick.get_axis(i)
+            				print "Axis: {} value: {}".format(i, axis)
+            			buttons = joystick.get_numbuttons()
+            			print "buttons {}".format(buttons)
+            			for i in range(buttons):
+            				button = joystick.get_button(i)
+            				print "Button: {} value: {}".format(i, button)
+            			hats = joystick.get_numhats()
+            			print "hats {}".format(hats)
+            			for i in range(hats):
+            				hat = joystick.get_hat(i)
+            				print "Hat: {} value: {}",format(i, hat)
 			self.state.update()
 
 def main():
