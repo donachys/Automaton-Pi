7192589543fc0ac0d6bef86ef9cf93ab15024571
Shaun Donachy <donachys@gmail.com>
2014-09-11 01:37:44 +0000

    swap forward and reverse states, update signal values, send stop signal once only, wait 2500ms before exiting neutral

--- automatonpi/states.py
+++ automatonpi/states.py
@@ -2,9 +2,10 @@
 
 from automatonpi.packages.adafruit.Adafruit_PWM_Servo_Driver import PWM
 import time
+import pygame
 from pygame.locals import *
 class CarState():
-    """docsring for CarState"""
+    """docstring for CarState"""
     pwm = PWM(0x40, debug=False)
     pwm.setPWMFreq(60)
     MOTORCHANNEL = 9
@@ -29,8 +30,8 @@ class CarState():
 
 class ForwardState(CarState):
     """docstring for ForwardState"""
-    MINFORWARD = 420
-    MAXFORWARD = 550
+    MINFORWARD = 395
+    MAXFORWARD = 250
     def __init__(self, f, s, myApp):
         self.force = f
         self.steer_val = s
@@ -40,7 +41,7 @@ class ForwardState(CarState):
             self.force = 100
         elif(self.force < 0):
             self.force = 0
-        tick = int(((self.force/100.00) * (self.MAXFORWARD - self.MINFORWARD))+self.MINFORWARD)
+        tick = int(self.MINFORWARD-((abs(self.force)/100.00) * (self.MINFORWARD - self.MAXFORWARD)))
         self.pwm.setPWM(self.MOTORCHANNEL, 0, tick)
         #super(CarState,self).steer(self.steer_val)
         self.steer(self.steer_val)
@@ -59,8 +60,8 @@ class ForwardState(CarState):
 
 class ReverseState(CarState):
     """docstring for ReverseState"""
-    MINREVERSE = 380
-    MAXREVERSE = 250
+    MINREVERSE = 415
+    MAXREVERSE = 550
     def __init__(self, f, s, myApp):
         self.force = f
         self.steer_val = s
@@ -70,7 +71,7 @@ class ReverseState(CarState):
             self.force = 100
         elif(self.force < 0):
             self.force = 0
-        tick = int(self.MINREVERSE-((abs(self.force)/100.00) * (self.MINREVERSE - self.MAXREVERSE)))
+        tick = int(((self.force/100.00) * (self.MAXREVERSE - self.MINREVERSE))+self.MINREVERSE)
         self.pwm.setPWM(self.MOTORCHANNEL, 0, tick)
         #super(CarState,self).steer(self.steer_val)
         self.steer(self.steer_val)
@@ -93,18 +94,24 @@ class NeutralState(CarState):
     def __init__(self, s, myApp):
         self.steer_val = s
         self.app = myApp
+        self.time_in_state = pygame.time.get_ticks()
+        self.sent_stop = False
     def update(self):
-        self.pwm.setPWM(self.MOTORCHANNEL, 0, self.STOPPEDTICK)
+        if not self.sent_stop:
+            self.pwm.setPWM(self.MOTORCHANNEL, 0, self.STOPPEDTICK)
         #super(CarState,self).steer(self.steer_val)
         self.steer(self.steer_val)
+        #time.sleep(.5)
     def handleInput(self, event):
         if event.type == KEYDOWN:
             if event.key in (K_UP, K_w):
                 #change state to forward
-                self.app.state = ForwardState(10, self.steer_val, self.app)
+                if (pygame.time.get_ticks() - self.time_in_state > 2500):
+                    self.app.state = ForwardState(10, self.steer_val, self.app)
             elif event.key in (K_DOWN, K_s):
                 #change state to reverse
-                self.app.state = ReverseState(10,self.steer_val, self.app)
+                if (pygame.time.get_ticks() - self.time_in_state > 2500):
+                    self.app.state = ReverseState(10,self.steer_val, self.app)
             elif event.key in (K_LEFT, K_a):
                 self.steer_val = -70
             elif event.key in (K_RIGHT, K_d):
