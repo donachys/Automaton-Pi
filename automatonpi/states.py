3d16c2c860acaf9d46d0bad3d16379f40340aaf8
Shaun Donachy <donachys@gmail.com>
2014-09-19 20:19:59 -0400

    correct setting of force and state in forwardstate

--- automatonpi/states.py
+++ automatonpi/states.py
@@ -60,9 +60,9 @@ class ForwardState(CarState):
 		if event.type == JOYAXISMOTION and event.axis <= 3:
 			if event.axis == 0:
 				if event.value <= 0:
-					self.force = NeutralState(self.steer_val, self.app)
+					self.app.state = NeutralState(self.steer_val, self.app)
 				elif event.value >= 0:
-					self.app.state = int(100 * event.value)
+					self.force = int(100 * event.value)
 			if event.axis == 3:
 				self.steer_val = int(100 * abs(event.value))
 
