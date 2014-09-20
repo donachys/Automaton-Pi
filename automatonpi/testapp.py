8090ed0c616c96af2b0f9f1b4c638a62d097e056
Shaun Donachy <donachys@gmail.com>
2014-09-20 10:55:59 -0400

    set neutral state if no joystick found

--- automatonpi/testapp.py
+++ automatonpi/testapp.py
@@ -29,27 +29,29 @@ class TestApp:
 		pygame.event.set_allowed(None)
 		pygame.event.set_allowed([pygame.QUIT, pygame.JOYAXISMOTION]) # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
 		while True:
-			for event in pygame.event.get(): # event handling loop
-				self.state.handleInput(event)
-				#name = joystick.get_name()
-				#print name
-				#axes = joystick.get_numaxes()
-				#print "axes {}".format(axes)
-				#for i in range(axes):
-				#	axis = joystick.get_axis(i)
-				#	print "Axis: {} value: {}".format(i, axis)
-				#buttons = joystick.get_numbuttons()
-				#print "buttons {}".format(buttons)
-				#for i in range(buttons):
-				#	button = joystick.get_button(i)
-				#	print "Button: {} value: {}".format(i, button)
-				#hats = joystick.get_numhats()
-				#print "hats {}".format(hats)
-				#for i in range(hats):
-				#	hat = joystick.get_hat(i)
-				#	print "Hat: {} value: {}",format(i, hat)
-			self.state.update()
-
+			if pygame.joystick.get_count() > 1:
+				for event in pygame.event.get(): # event handling loop
+					self.state.handleInput(event)
+					#name = joystick.get_name()
+					#print name
+					#axes = joystick.get_numaxes()
+					#print "axes {}".format(axes)
+					#for i in range(axes):
+					#	axis = joystick.get_axis(i)
+					#	print "Axis: {} value: {}".format(i, axis)
+					#buttons = joystick.get_numbuttons()
+					#print "buttons {}".format(buttons)
+					#for i in range(buttons):
+					#	button = joystick.get_button(i)
+					#	print "Button: {} value: {}".format(i, button)
+					#hats = joystick.get_numhats()
+					#print "hats {}".format(hats)
+					#for i in range(hats):
+					#	hat = joystick.get_hat(i)
+					#	print "Hat: {} value: {}",format(i, hat)
+				self.state.update()
+			else:
+				self.state = NeutralState(0, self)
 def main():
 	app = TestApp()
 	app.run()
