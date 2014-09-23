24e75bdfa43fdc1306a2327b5bb9e44520e5f260
Shaun Donachy <donachys@gmail.com>
2014-09-23 19:02:52 -0400

    spaces

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='Automaton Pi',
-	  version=0.1,
-	  description=('Autonomous control of the Helion Animus 18TR using a '
-				   'Raspberry PI.'),
-	  author='Shaun Donachy, Ljiljana Zigic, Mack Stump',
-	  author_email=('donachys@gmail.com, zigicljiljana@gmail.com, '
-					'mack.stump@gmail.com'),
-	  packages=['automatonpi', 'automatonpi.packages.adafruit'],
-	  license='GPLv3',
-	  scripts=['bin/automatonpi'])
+      version=0.1,
+      description=('Autonomous control of the Helion Animus 18TR using a '
+                   'Raspberry PI.'),
+      author='Shaun Donachy, Ljiljana Zigic, Mack Stump',
+      author_email=('donachys@gmail.com, zigicljiljana@gmail.com, '
+                    'mack.stump@gmail.com'),
+      packages=['automatonpi', 'automatonpi.packages.adafruit'],
+      license='GPLv3',
+      scripts=['bin/automatonpi'])
